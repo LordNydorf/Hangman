a43962f5f5d8af1a0af673dd552259847309b777
Rohit Krishnan <95649278+LordNydorf@users.noreply.github.com>
2024-07-25 20:49:41 +0530

    Add files via upload

---
+++ mistakeStatus.kt
@@ -0,0 +1,78 @@
+package com.home.hangman
+
+fun print0Mistakes(){
+    println("  |------|-")
+    println("  |      | ")
+    println("  |        ")
+    println("  |        ")
+    println("  |        ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print1Mistake(){
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      0 ")
+    println("  |        ")
+    println("  |        ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print2Mistakes(){
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      0 ")
+    println("  |      | ")
+    println("  |      | ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print3Mistakes(){
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      0 ")
+    println("  |     /| ")
+    println("  |      | ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print4Mistakes(){
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      0 ")
+    println("  |     /|\\ ")
+    println("  |      | ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print5Mistakes(){
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      0 ")
+    println("  |     /|\\ ")
+    println("  |      | ")
+    println("  |     /  ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print6Mistakes(){
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      0 ")
+    println("  |     /|\\ ")
+    println("  |      | ")
+    println("  |     / \\")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
---
+++ printGameStatus.kt
@@ -0,0 +1,18 @@
+package com.home.hangman
+
+fun printGameStatus(){
+    when(mistakes) {
+        0 -> print0Mistakes()
+        1 -> print1Mistake()
+        2 -> print2Mistakes()
+        3 -> print3Mistakes()
+        4 -> print4Mistakes()
+        5 -> print5Mistakes()
+        6 -> print6Mistakes()
+    }
+
+    print("Word: ")
+    for (element in guesses)
+        print("$element ")
+    println("\nYou have $remainingGuesses guess(es) left")
+}
